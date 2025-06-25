0b8f8a52d2749f0002b47ca422749aca0d356370
Garret Friesen <138935870+AiAutomatrix@users.noreply.github.com>
2025-06-25 17:45:40 +0000

    Set iframe height to 90hv for better layout control

--- src/app/demos/personalized-bot/page.tsx
+++ src/app/demos/personalized-bot/page.tsx
@@ -4,9 +4,9 @@ import * as React from "react";
 export default function IframePage() {
   return (
     
-      <iframe
+       <iframe
         src="https://tradeflow-chi.vercel.app"
-        style={{ width: "100%", height: "80hv" }}
+        style={{ width: "100%", height: "90vh" }}
         title="Tradeflow App"
       />
     
