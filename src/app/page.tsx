312bfba537753b9b5f4e9b8308e964e17a0bbf9a
Garret Friesen <garretfriesen1@gmail.com>
2025-08-24 19:03:02 +0000

    comment out the OUR WORK section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <main className="flex-1">
         <HeroSection />
         <ServicesSection />
-        <GallerySection />
+        {/* <GallerySection /> */}
         <AboutSection />
         <TestimonialsSection />
         <SeoToolSection />
