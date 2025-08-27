469be2eca2e40e364607ce5e3402219bb77b3312
Garret Friesen <garretfriesen1@gmail.com>
2025-08-27 01:32:25 +0000

    i can't see the webchat on the demo page iframe container

--- src/app/bot-demos/customer-service/webchat.tsx
+++ src/app/bot-demos/customer-service/webchat.tsx
@@ -1,13 +1,30 @@
-export default function CustomerServiceWebchat() {
-    const embedCode = `
-      <script src="https://cdn.botpress.cloud/webchat/v3.2/inject.js"></script>
-      <script src="https://files.bpcontent.cloud/2025/08/26/22/20250826221413-HL4C5ZIZ.js" defer></script>
-    `;
 
-    return (
-        <div 
-            className="w-full h-[600px] md:h-[700px]"
-            dangerouslySetInnerHTML={{ __html: embedCode }} 
-        />
-    );
+"use client";
+
+import React, { useEffect } from 'react';
+
+export default function CustomerServiceWebchat() {
+  useEffect(() => {
+    const injectScript = document.createElement('script');
+    injectScript.src = "https://cdn.botpress.cloud/webchat/v3.2/inject.js";
+    injectScript.async = true;
+    document.body.appendChild(injectScript);
+
+    const configScript = document.createElement('script');
+    configScript.src = "https://files.bpcontent.cloud/2025/08/26/22/20250826221413-HL4C5ZIZ.js";
+    configScript.defer = true;
+    document.body.appendChild(configScript);
+
+    return () => {
+      // Cleanup the scripts when the component unmounts
+      document.body.removeChild(injectScript);
+      document.body.removeChild(configScript);
+    }
+  }, []);
+
+  return (
+    <div className="w-full h-[600px] md:h-[700px] flex items-center justify-center">
+      {/* The webchat will be injected into the page by the scripts */}
+    </div>
+  );
 }
--- src/components/layout/bot-demo-page-layout.tsx
+++ src/components/layout/bot-demo-page-layout.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 import React from "react";
 
 interface BotDemoPageLayoutProps {
